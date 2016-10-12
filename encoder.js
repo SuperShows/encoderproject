0f9888189399322111268b134473ad85d7f4985b
Jake Young <cheetopuffgaming@gmail.com>
2016-10-12 10:40:08 -0700

    made it check one at a time

--- encoder.js
+++ encoder.js
@@ -1,4 +1,9 @@
-$("#inputText").keydown(function(e) {
+$("#inputText").keyup(function(e) {
   var inputText = document.getElementById("inputText").value;
   document.getElementById("textArea").innerHTML = inputText;
+});
+
+$("input[type='radio']").click(function() {
+  $("input:checked").prop('checked', false );
+  $(this).prop('checked', true )
 });
